7a682b8df631f79103c8e33ead47e54da7b62ca8
Mateus Abdallah <abdallahmateus@gmail.com>
2023-05-19 21:23:09 -0700

    set the frame constructor and paint method

--- MyFrame.java
+++ MyFrame.java
@@ -1,5 +1,43 @@
+
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
-
+	
+	Image image;
+	Graphics graphics;
+	Box player;
+	Box enemy;
+	boolean gameOver;
+	
+	MyFrame(){
+		
+		player = new Box(100, 300, 50, 50, Color.blue);
+		enemy = new Box(500, 300, 50, 50, Color.red);
+		gameOver = false;
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(600, 600);
+		this.setVisible(true);
+		//this.addKeyListener(new AL());
+		
+	}
+	
+	public void paint(Graphics g) {
+		image = createImage(this.getWidth(), this.getHeight());
+		graphics = image.getGraphics();
+		g.drawImage(image,0,0,this);
+		
+		player.draw(g);
+		enemy.draw(g);
+	}
+	
+	public class AL extends KeyAdapter {
+		
+	}
+	
 }
+	
+	
