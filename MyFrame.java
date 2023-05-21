9ec38faa2384f977f9bc5588b17455f1f639a915
Mateus Abdallah <abdallahmateus@gmail.com>
2023-05-21 10:18:40 -0700

    check collision with the intersects method

--- MyFrame.java
+++ MyFrame.java
@@ -21,7 +21,7 @@ public class MyFrame extends JFrame {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(600, 600);
 		this.setVisible(true);
-		//this.addKeyListener(new AL());
+		this.addKeyListener(new AL());
 		
 	}
 	
@@ -35,13 +35,25 @@ public class MyFrame extends JFrame {
 		
 		if(gameOver) {
 			g.setColor(Color.RED);
-			g.setFont(new Font("Sans-serif", Font.PLAIN, 45));
+			g.setFont(new Font("Calibri", Font.PLAIN, 45));
 			g.drawString("Game Over", 150, 100);
 		}
 	}
 	
+	public void checkCollision() {
+		if(player.intersects(enemy)) {
+			gameOver = true;
+			System.out.println("GAME OVER");
+		}
+	}
+	
 	public class AL extends KeyAdapter {
-		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			player.keyPressed(e);
+			checkCollision();
+			repaint();
+		}
 	}
 	
 }
